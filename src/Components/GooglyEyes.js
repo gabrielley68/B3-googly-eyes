9855691547d0d6b8e8ed60381aad975a6b5d32f9
Ley Gabriel <gabriel.ley@outlook.fr>
2019-01-31 08:41:28 +0100

    Commit initial

---
+++ src/Components/GooglyEyes.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import '../Stylesheets/App.css';
+
+class GooglyEyes extends Component {
+    constructor()
+    {
+        super();
+    }
+
+    render(){
+        return(
+          <canvas className="GooglyEyes">
+
+          </canvas>
+        );
+    }
+}
+
+export default GooglyEyes;
