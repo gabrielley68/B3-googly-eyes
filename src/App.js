e8e5d90d8cdfa46e792dbcd8df210655e1cdae3c
Ley Gabriel <gabriel.ley@outlook.fr>
2019-01-31 08:42:38 +0100

    Commit initial (good)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import GooglyEyes from './Components/GooglyEyes';
+import './Stylesheets/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <GooglyEyes/>
+            </div>
+        );
+    }
+}
+
+export default App;
